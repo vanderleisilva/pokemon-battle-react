f265260470c4ed01c4cf27e10580f27b38f66e75
Vanderlei Alves da Silva <vanderleisilva@santos.sp.gov.br>
2017-11-13 10:15:37 -0200

    Test renders condition on player list component

--- src/components/playerList/playerList.test.js
+++ src/components/playerList/playerList.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import PlayerList from './index.js';
 import FakePlayers from '../../constants/dummy-data/players';
+
+configure({ adapter: new Adapter() });
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<PlayerList players={FakePlayers} />, div);
+});
+
+it('should render a list of thumbs for each players', () => {
+	const wrapper = shallow(<PlayerList players={FakePlayers}/>);
+  	expect(wrapper.find('.thumbnail').length).toEqual(FakePlayers.length);
 });
